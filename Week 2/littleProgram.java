318d486f181d227c0c4b75e9081cc4f50e2b82e5
Ben Puryear <bpuryear@zagmail.gonzaga.edu>
2022-01-19 18:48:20 +0000

    added devcontainer

--- Week 2/littleProgram.java
+++ Week 2/littleProgram.java
@@ -2,17 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Person a = new Person();
-
+        a.setHeight_m(2.0);
+        // a.height_m = 3.0; Doesnt work since height_m is private
         a.printHeight();
     }
 }
 
 class Person {
-    double height_m;
-    double weight_kg;
+    private double height_m;
 
-    void setHeight_m(double height_m) {
-        this.height_m = height_m;
+    void setHeight_m(double newHeight_m) {
+        if (newHeight_m >= 3) { // taller than 3 meters? i dont think so
+            this.height_m = 0.0;
+        } else {
+            this.height_m = newHeight_m;
+        }
+
     }
 
     void printHeight() {
